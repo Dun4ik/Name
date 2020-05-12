d65fe0753553dc61f3bfe6fe6f94cdf3578ff3ac
Dun4ik <emil.miftakhov92@gmail.com>
2020-05-12 20:45:24 +0300

    LOVE this world

--- lib/main2.dart
+++ lib/main2.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
-
-void main() {
-  int a;
-  int b;
-  runApp(new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: new Scaffold(
-        appBar: new AppBar(
-            title: new Text('Just not click on me'),
-            backgroundColor: Colors.orange),
-        body: new MyBody(),
-      )));
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: new Scaffold(
+          appBar: new AppBar(
+              title: new Text('Just not click on me'),
+              backgroundColor: Colors.orange),
+          body: new MyBody(),
+    ));
+  }
 }
 
 class MyBody extends StatelessWidget {
@@ -21,43 +24,43 @@ class MyBody extends StatelessWidget {
     return Stack(children: [
       new Center(
           child: new Column(children: [
-        new TextField(
-          obscureText: false,
-          decoration: InputDecoration(
-              border: InputBorder.none, labelText: 'Enter your message here'),
-        ),
-        new Text('Hello, my friend'),
-        new FlatButton(
-            onPressed: () async {
-              const url = 'https://yummyanime.club';
-              if (await canLaunch(url)) {
-                await launch(url);
-              } else {
-                throw 'Sorry, but your link is not found';
-              }
-            },
-            child: new Text('JUST PUSH ME!'),
-            color: Colors.red,
-            textColor: Colors.white),
-        FlatButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => Secondscreen()));
-          },
-          child: Text('Other screen'),
-          color: Colors.black54,
-          textColor: Colors.lightGreenAccent,
-        ),
-        FlatButton(
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => Calculator()));
-          },
-          child: Text('Calculator'),
-          color: Colors.green,
-          textColor: Colors.black,
-        )
-      ])),
+            new TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                  border: InputBorder.none, labelText: 'Enter your message here'),
+            ),
+            new Text('Hello, my friend'),
+            new FlatButton(
+                onPressed: () async {
+                  const url = 'https://yummyanime.club';
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  } else {
+                    throw 'Sorry, but your link is not found';
+                  }
+                },
+                child: new Text('JUST PUSH ME!'),
+                color: Colors.red,
+                textColor: Colors.white),
+            FlatButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Secondscreen()));
+              },
+              child: Text('Other screen'),
+              color: Colors.black54,
+              textColor: Colors.lightGreenAccent,
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Calculator()));
+              },
+              child: Text('Calculator'),
+              color: Colors.green,
+              textColor: Colors.black,
+            )
+          ])),
       new Padding(
           padding: EdgeInsets.only(left: 50.0, top: 200),
           child: new IconButton(
@@ -166,19 +169,26 @@ class Calculator extends StatefulWidget {
 
 class _CalculatorState extends State<Calculator> {
   String value = '';
-  String a1='';
-  String m='';
-  String a2='';
+  String a1 = '';
+  String m = '';
+  String a2 = '';
+
   void ans(String b) {
-    double result=0;
-    if(m=='+') result=double.parse(a1) + double.parse(a2);
-    else if(m=='-') result=double.parse(a1) - double.parse(a2);
-    else if(m=='*') result=double.parse(a1) * double.parse(a2);
-    else result=double.parse(a1) / double.parse(a2);
-    if(result.toInt().toDouble()== result) a1=result.toInt().toString();
-    else a1=result.toString();
+    double result = 0;
+    if (m == '+')
+      result = double.parse(a1) + double.parse(a2);
+    else if (m == '-')
+      result = double.parse(a1) - double.parse(a2);
+    else if (m == '*')
+      result = double.parse(a1) * double.parse(a2);
+    else
+      result = double.parse(a1) / double.parse(a2);
+    if (result.toInt().toDouble() == result)
+      a1 = result.toInt().toString();
+    else
+      a1 = result.toString();
     setState(() {
-      value=a1;
+      value = a1;
     });
   }
 
@@ -188,7 +198,7 @@ class _CalculatorState extends State<Calculator> {
     return Scaffold(
         appBar: AppBar(
           title: Text('Calculator'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.indigo,
         ),
         body: Column(children: [
           Container(
@@ -199,12 +209,16 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        a2+='1';
-                      else a1+='1';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/'))
+                        a2 += '1';
+                      else
+                        a1 += '1';
                       setState(() {
                         value += '1';
                       });
@@ -215,12 +229,16 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        a2+='2';
-                      else a1+='2';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/'))
+                        a2 += '2';
+                      else
+                        a1 += '2';
                       setState(() {
                         value += '2';
                       });
@@ -231,12 +249,16 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        a2+='3';
-                      else a1+='3';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/'))
+                        a2 += '3';
+                      else
+                        a1 += '3';
                       setState(() {
                         value += '3';
                       });
@@ -247,12 +269,14 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        ans(value);
-                      m='+';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/')) ans(value);
+                      m = '+';
                       setState(() {
                         if (!value.contains('+')) value += '+';
                       });
@@ -267,12 +291,16 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        a2+='4';
-                      else a1+='4';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/'))
+                        a2 += '4';
+                      else
+                        a1 += '4';
                       setState(() {
                         value += '4';
                       });
@@ -283,12 +311,16 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        a2+='5';
-                      else a1+='5';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/'))
+                        a2 += '5';
+                      else
+                        a1 += '5';
                       setState(() {
                         value += '5';
                       });
@@ -299,12 +331,16 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        a2+='6';
-                      else a1+='6';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/'))
+                        a2 += '6';
+                      else
+                        a1 += '6';
                       setState(() {
                         value += '6';
                       });
@@ -315,12 +351,14 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        ans(value);
-                      m='-';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/')) ans(value);
+                      m = '-';
                       setState(() {
                         if (!value.contains('-')) value += '-';
                       });
@@ -335,12 +373,16 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        a2+='7';
-                      else a1+='7';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/'))
+                        a2 += '7';
+                      else
+                        a1 += '7';
                       setState(() {
                         value += '7';
                       });
@@ -351,12 +393,16 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        a2+='8';
-                      else a1+='8';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/'))
+                        a2 += '8';
+                      else
+                        a1 += '8';
                       setState(() {
                         value += '8';
                       });
@@ -367,12 +413,16 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        a2+='9';
-                      else a1+='9';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/'))
+                        a2 += '9';
+                      else
+                        a1 += '9';
                       setState(() {
                         value += '9';
                       });
@@ -383,12 +433,14 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        ans(value);
-                      m='*';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/')) ans(value);
+                      m = '*';
                       setState(() {
                         if (!value.contains('*')) value += '*';
                       });
@@ -403,14 +455,14 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
                       setState(() {
                         value = '';
-                        a1='';
-                        a2='';
-                        m='';
+                        a1 = '';
+                        a2 = '';
+                        m = '';
                       });
                     },
                     child: Text('C'),
@@ -419,12 +471,16 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        a2+='0';
-                      else a1+='0';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/'))
+                        a2 += '0';
+                      else
+                        a1 += '0';
                       setState(() {
                         value += '0';
                       });
@@ -435,7 +491,7 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
                       ans(value);
@@ -446,12 +502,14 @@ class _CalculatorState extends State<Calculator> {
                   height: screen_h,
                   width: screen_w / 4,
                   child: FlatButton(
-                    color: Colors.deepOrange,
+                    color: Colors.deepPurple,
                     textColor: Colors.white,
                     onPressed: () {
-                      if(value.contains('+') || value.contains('-') || value.contains('*') || value.contains('/'))
-                        ans(value);
-                      m='/';
+                      if (value.contains('+') ||
+                          value.contains('-') ||
+                          value.contains('*') ||
+                          value.contains('/')) ans(value);
+                      m = '/';
                       setState(() {
                         if (!value.contains('/')) value += '/';
                       });
